a656a7ab8d281f52bfb1554e786df6e2a7e46c55
milind31 <69882587+milind31@users.noreply.github.com>
2020-10-28 21:19:56 -0700

    Add files via upload

---
+++ age_detection.py
@@ -0,0 +1,40 @@
+#find and get position of face from webcame footage using frontalface haar cascade
+#feed face to DeepFace model to detect race
+#display result of model on screen
+from deepface import DeepFace
+import cv2
+import numpy as np
+
+#open webcam and cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+while True:
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5) #find face using cascade
+        for (x, y, w, h) in faces:
+                detected_face = img[int(y):int(y+h), int(x):int(x+w)]
+                margin_rate = 30
+                #get some area around face to match position of face on images the model is trained on
+                try:
+                        margin_x = int(w * margin_rate / 100)
+                        margin_y = int(h * margin_rate / 100)
+                        detected_face = img[int(y-margin_y):int(y+h+margin_y), int(x-margin_x):int(x+w+margin_x)]
+                        detected_face = cv2.resize(detected_face, (224, 224)) #resize to 224x224
+                except Exception as err:
+                        detected_face = img[int(y):int(y+h), int(x):int(x+w)]
+                        detected_face = cv2.resize(detected_face, (224, 224))
+                obj = DeepFace.analyze(detected_face, actions=['age'], enforce_detection=False) #feed to model
+                cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 1) #draw rectangle
+                display_string = str(round(obj["age"]))
+                cv2.putText(img, display_string, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (36,255,12), 1, cv2.LINE_AA) #add text
+                
+        cv2.imshow('img', img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+                break
+
+cap.release()
+cv2.destroyAllWindows()
+
